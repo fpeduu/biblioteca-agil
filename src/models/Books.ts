73d9e9132bbb7f8e90adec9a944c2b5afe9942a0
fpeduu <filipeed14@gmail.com>
2022-07-03 22:43:59 -0300

    starting database

---
+++ src/models/Books.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Books {
+/*
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    name: string
+
+    @Column()
+    description: string
+
+    @Column()
+    referrer: string
+
+    @Column()
+    phone: string*/
+}
