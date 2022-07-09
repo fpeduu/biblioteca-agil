3c9fa113fa83b75094e075cb242f3ac7a504855e
fpeduu <filipeed14@gmail.com>
2022-07-09 17:28:00 -0300

    fixing db script

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('../database/db.json'); // <== Will be created later
+const router = jsonServer.router('./src/database/db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = 3001; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
