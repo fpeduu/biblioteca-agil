90fff07ccc4460488611b89288dd077459977d72
fpeduu <filipeed14@gmail.com>
2022-07-09 17:38:36 -0300

    usign external db

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/books'
+    baseURL: 'https://my-json-server.typicode.com/fpeduu/biblioteca-agil-db/books'
 });
 
 export default api;
